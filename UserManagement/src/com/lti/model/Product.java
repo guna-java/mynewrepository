c8b91499a9624a73037727b124471fa234a1dc5e
Guna <Guna@DESKTOP-TDELNE0>
2020-10-16 09:27:29 +0530

    added prod class to new branch

---
+++ UserManagement/src/com/lti/model/Product.java
@@ -0,0 +1,28 @@
+package com.lti.model;
+
+public class Product {
+	int prodId;
+	String prodName;
+	String prodDescription;
+	public int getProdId() {
+		return prodId;
+	}
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	public String getProdDescription() {
+		return prodDescription;
+	}
+	public void setProdDescription(String prodDescription) {
+		this.prodDescription = prodDescription;
+	}
+	
+	
+
+}
